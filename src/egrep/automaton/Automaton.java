4e1eeab641b1ec3886d23c4ec947d12b45779270
EmilieSiau <emilie.siau@gmail.com>
2021-09-29 08:34:23 +0200

    Automaton

--- src/egrep/automaton/Automaton.java
+++ src/egrep/automaton/Automaton.java
@@ -1,5 +1,35 @@
 package egrep.automaton;
 
+import egrep.parser.RegExTree;
+
+import java.lang.reflect.Array;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * An automaton is represented by a Map of keys:NodeId and values:[259 NodeId]
+ */
 public class Automaton {
+
+    // ----- Attributes -----
+
+    private Map<NodeId, NodeId[]> automaton;
+    private RegExTree tree;
+    private int cptNodes;
+
+    // ----- Constructors -----
+
+    public Automaton(RegExTree tree) {
+        this.tree = tree;
+        automaton = new HashMap<>();
+        cptNodes = 0;
+        process();
+    }
+
+    public void process() {
+
+    }
 
 }
